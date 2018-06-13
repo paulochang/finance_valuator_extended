d077fc6578ae2a31f31bc0eb4af3c956ec1385d3
Paulo Chang <pigenio@gmail.com>
2018-06-13 02:21:19 +0200

    Add fixed leg price test Add floating forward rates test and implementation.

--- assignment1/src/Leg/FloatingLeg.cpp
+++ assignment1/src/Leg/FloatingLeg.cpp
@@ -8,14 +8,32 @@
  */
 
 
-//TODO: Implement actual floating cash flow
 std::vector<double> FloatingLeg::getLegCashFlows(std::vector<double> dayCountFractionVector) {
     std::vector<double> legCashFlows{};
     for (int i = 0; i < dayCountFractionVector.size(); ++i) {
-
     }
-    //for (auto currentFraction : dayCountFractionVector)
-    //    legCashFlows.emplace_back(m_rate * currentFraction * m_notional);
-
     return legCashFlows;
+}
+
+std::vector<double> FloatingLeg::getForwardRatesVector(std::vector<double> totalDayCountFractionVector) {
+
+    std::vector<double> forwardRatesVector{};
+    forwardRatesVector.reserve(m_payingDates.size() - 1);
+    double pastTotalDayCountFraction = 0;
+
+    for (unsigned int i = 1; i < m_payingDates.size(); i++) {
+        double currentRate = m_zeroCouponCurve.getRateFromDate(m_payingDates.at(i));
+        double currentTotalDayCountFraction = totalDayCountFractionVector.at(i - 1);
+        double pastRate = m_zeroCouponCurve.getRateFromDate(m_payingDates.at(i - 1));
+        pastRate = isnan(pastRate) ? 0 : pastRate;
+
+        double currentFwdRate =
+                (currentRate * currentTotalDayCountFraction -
+                 pastRate * pastTotalDayCountFraction)
+                / (currentTotalDayCountFraction - pastTotalDayCountFraction);
+        forwardRatesVector.emplace_back(currentFwdRate);
+        pastTotalDayCountFraction = currentTotalDayCountFraction;
+    }
+
+    return forwardRatesVector;
 }
--- assignment1/src/Leg/FloatingLeg.h
+++ assignment1/src/Leg/FloatingLeg.h
@@ -6,22 +6,24 @@
 #define SQF_FLOATINGLEG_H
 
 #include <Leg/ZeroCurve/ZeroCouponCurve.h>
+#include <algorithm>
 #include "Leg.h"
 
 //Clase que implementa una pata flotante
 class FloatingLeg : public Leg {
-
-    ZeroCouponCurve m_index;
+    int periodsPerYear;
 public:
 
     // Constructor (LLamamos al constructor de la superclase)
     FloatingLeg(double notional, double rates, std::vector<boost::gregorian::date> referenceDates,
-                DayCountCalculator &dayCalculator, ZeroCouponCurve &zeroCouponCurve, ZeroCouponCurve index) :
+                DayCountCalculator &dayCalculator, ZeroCouponCurve &zeroCouponCurve, int periodsPerYear) :
             Leg(notional, rates, std::move(referenceDates), dayCalculator, zeroCouponCurve) {
-        m_index = std::move(index);
+        this->periodsPerYear = periodsPerYear;
     }
 
     std::vector<double> getLegCashFlows(std::vector<double> dayCountFractionVector) override;
+
+    std::vector<double> getForwardRatesVector(std::vector<double> totalDayCountFractionVector);
 
 };
 
